3b86f47fb143ee41a6c9d2360c354d5c94dce36e
Angélina Droz <angieso91@gmail.com>
2026-03-16 21:49:22 +0100

    explicite return type for model relation (Eloquent convention)

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Booking extends Model
 {
@@ -14,11 +15,11 @@ class Booking extends Model
         'note',
     ];
 
-     public function property()
+    public function property(): BelongsTo
     {
         return $this->belongsTo(Property::class);
     }
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
